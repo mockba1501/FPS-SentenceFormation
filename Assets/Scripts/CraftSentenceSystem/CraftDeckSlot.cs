68a455a76d6b89ce9605134a948d9df6e613f335
Yoshio-0 <90753067+Yoshio-0@users.noreply.github.com>
2022-10-27 20:34:38 +0300

    Connected panels with script (Picture, Inventory, Craft deck)

--- Assets/Scripts/CraftSentenceSystem/CraftDeckSlot.cs
+++ Assets/Scripts/CraftSentenceSystem/CraftDeckSlot.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CraftDeckSlot : CraftDeck
 {
+    public GameObject craftDeckSlot;
+    public TMPro.TMP_Text wordInItem;
     public CraftDeckSlotScriptableObject craftDeckSlotSO;
 
     public void Start()
@@ -15,6 +19,8 @@ public class CraftDeckSlot : CraftDeck
     {
         if (craftDeckSlotSO.itemInCraftDeckSlot)
         {
+            craftDeckSlot.GetComponent<Image>().sprite = craftDeckSlotSO.itemInCraftDeckSlot.itemSprite;
+            wordInItem.text = craftDeckSlotSO.itemInCraftDeckSlot.word;
             Debug.Log("CraftDeckSlotID: " + craftDeckSlotSO.craftDeckSlotId + "\n" + "Item in CraftDeckSlot: " + craftDeckSlotSO.itemInCraftDeckSlot.itemId); // Just a test to show itemID of a item contended in craftDeckClot. 
         }
     }
--- Assets/Scripts/CraftSentenceSystem/InventorySlot.cs
+++ Assets/Scripts/CraftSentenceSystem/InventorySlot.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class InventorySlot : Inventory
 {
+    public GameObject inventorySlot;
+    public TMPro.TMP_Text wordInItem;
     public InventorySlotScriptableObject inventorySlotSO;
 
     public void Start()
@@ -15,6 +19,9 @@ public class InventorySlot : Inventory
     {
         if (inventorySlotSO.itemInInventorySlot) 
         {
+            inventorySlot.GetComponent<Image>().sprite = inventorySlotSO.itemInInventorySlot.itemSprite;
+            wordInItem.text = inventorySlotSO.itemInInventorySlot.word;
+
             Debug.Log("InventorySlotID: " + inventorySlotSO.inventorySlotId  + "\n" + "Item in InventorySlot: " + inventorySlotSO.itemInInventorySlot.itemId); // Just a test to show itemID of a item contended in inventory slot. 
         }
     }
